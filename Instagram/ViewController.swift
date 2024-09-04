1df9682d73fb308f1444a7df3dfbd4ad6283384e
kobuki <sota_kobuki@r.recruit.co.jp>
2024-09-04 17:01:45 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 小吹　創大 on 2024/09/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
